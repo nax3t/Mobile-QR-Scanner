5c1c5ab3bd5549454899ed3d3de48bfd75785c90
Ian <iantskon@gmail.com>
2015-02-08 15:24:10 -0800

    Add working QR Scanner

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var qRScan = angular.module('starter', ['ionic', 'ngCordova']);
 
-.run(function($ionicPlatform) {
+qRScan.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +17,17 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+qRScan.controller("ExampleController", function($scope, $cordovaBarcodeScanner) {
+ 
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            alert(imageData.text);
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
+ 
+});
