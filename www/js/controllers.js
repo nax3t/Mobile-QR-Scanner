5bef6ac580db7b81106e02900b6c4043b7d2e37b
Ian <iantskon@gmail.com>
2015-02-11 18:22:29 -0800

    Add errors alert to registration form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,6 +16,20 @@ angular.module('myControllers', [])
 		});
 }])
 
+.controller('UserRegistrationsCtrl', ['$scope', '$location', '$auth', function($scope, $location, $auth) {
+	$scope.$on('auth:registration-email-error', function(ev, reason) {
+		$scope.error = reason.errors[0];
+	});
+	$scope.handleRegBtnClick = function() {
+		$auth.submitRegistration($scope.registrationForm).then(function() {
+			$auth.submitLogin({
+				email: $scope.registrationForm.email,
+				password: $scope.registrationForm.password
+			});
+		});
+	};
+}])
+
 .controller("qrCtrl", function($scope, $cordovaBarcodeScanner, $location, Attendee) {
   $scope.create = function() {
       $cordovaBarcodeScanner.scan().then(function(imageData) {
